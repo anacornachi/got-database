9036e69f79698b18bac725c9c050c794b6cd5cbc
Laíssa Fernandes <laissafermaria@gmail.com>
2022-02-02 20:06:59 -0300

    alterando a query

--- queries/consultas/casas_por_regiao.sql
+++ queries/consultas/casas_por_regiao.sql
@@ -1,5 +1,5 @@
 -- Quantas casas existem por região? 
-SELECT regiao, count(nome_da_casa) AS quantidade_casas
+SELECT regiao, COUNT(nome_da_casa) AS quantidade_casas
 FROM casas
 GROUP BY regiao
 ORDER BY quantidade_casas DESC;
