23924585aaccf6b0568e6ad79cfcdd2aeff75f2a
mjantunes2 <mjantunes09@gmail.com>
2022-02-02 19:21:34 -0300

    feat/estrutura/Murilo
    criação da tabela de personagens com padronização do SQL.

---
+++ queries/estrutura/create_table_personagens.sql
@@ -0,0 +1,9 @@
+-- Criação da tabela personagens;
+CREATE TABLE personagens
+( ID INT PRIMARY KEY,
+nome VARCHAR(100),
+ator_atriz VARCHAR(150),
+ep_participante INT,
+primeira_aparicao INT,
+ultima_aparicao INT
+);
