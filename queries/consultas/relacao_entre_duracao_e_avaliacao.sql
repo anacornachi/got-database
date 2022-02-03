d0e15bd2402ab16899940664db5df4843f5f6df0
anacornachi <anacorn4chi@gmail.com>
2022-02-03 16:13:30 -0300

    fix queries

--- queries/consultas/relacao_entre_duracao_e_avaliacao.sql
+++ queries/consultas/relacao_entre_duracao_e_avaliacao.sql
@@ -1,4 +1,4 @@
 -- Qual a relação entre duração de episodio e sua avaliação por temporadas?
-SELECT temporada, round(avg(duracao)) AS media_duracao, round(avg(avaliacao),2) AS media_avaliacao
+SELECT temporada, ROUND(AVG(duracao)) AS media_duracao, ROUND(AVG(avaliacao),2) AS media_avaliacao
 FROM episodios
 GROUP BY temporada ;
--- queries/consultas/temporada_melhor_avaliada.sql
+++ queries/consultas/temporada_melhor_avaliada.sql
@@ -1,5 +1,5 @@
 -- Quais foram as avaliações por temporada?
-SELECT temporada, round(avg(avaliacao),2) AS media_avaliacao
+SELECT temporada, ROUND(AVG(avaliacao),2) AS media_avaliacao
 FROM episodios
 GROUP BY temporada
 ORDER BY media_avaliacao DESC;
